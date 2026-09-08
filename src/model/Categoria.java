ab94b83098c64b8c4b7441a980263311f62673b6
Filipe Messias Silva <filipe.silva@transireserv.com>
2026-09-08 19:52:31 -0300

    Implementando interface e funcionalidades

--- src/model/Categoria.java
+++ src/model/Categoria.java
@@ -4,21 +4,6 @@ public class Categoria {
     private int id;
     private String nome;
 
-    // Criamos construtores vazios para não ter a necessidade de sempre ter os dados
-    // nas mãos para criar um objeto. Pense por exemplo quando o usuario vai preencher os campos aos poucos:
-
-    /*
-    *
-    * Produto prod = new Produto();
-    * prod.setName(campoNome.getString());
-    * prod.setDesricao(campoDescricao.getString());
-    * prod.setPreco(campoPreco.getString());
-    * */
-
-    // Além disso existem bibliotecas que exigem construtores vazios, pois usam o objeto em branco
-    // para depois usam os setters para preencher campo por campo.
-
-
     public Categoria(){
 
     }
--- src/view/TelaProduto.java
+++ src/view/TelaProduto.java
@@ -1,9 +1,15 @@
 package view;
 
+import dao.conexaoDB.CategoriaDAO;
+import dao.conexaoDB.ProdutoDAO;
 import model.Categoria;
+import model.Produto;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.List;
 
 public class TelaProduto extends JFrame {
 
@@ -11,16 +17,62 @@ public class TelaProduto extends JFrame {
     private JTextField descr_campo;
     private JTextField preco_campo;
     private JTextField qtd_campo;
-
     private JComboBox<Categoria> comboCategoria;
+
+    private JButton add_btn;
+    private JButton atua_btn;
+    private JButton excluir_btn;
+    private JButton limpar_btn;
+
+    private JTable tabelaProduto;
+    private DefaultTableModel modeloTabela;
+
+    private void carregarCategoria(){
+        try{
+            CategoriaDAO categoriaDAO = new CategoriaDAO();
+            List<Categoria> categorias = categoriaDAO.listarTodas();
+
+            for(Categoria categoria : categorias){
+                comboCategoria.addItem(categoria);
+            }
+        }catch(SQLException exc){
+            JOptionPane.showMessageDialog(this,"Erro ao carregar categoria: "+exc.getMessage());
+        }
+    }
+
+    private void carregarProdutos(){
+        try{
+            ProdutoDAO produtoDAO = new ProdutoDAO();
+            List<Produto> produtos = produtoDAO.listarTodos();
+
+            modeloTabela.setRowCount(0);
+
+            for(Produto produto : produtos){ // Object é um array que guarda variados tipos de objeto
+                Object[] linha = {
+                    produto.getId(),
+                    produto.getNome(),
+                    produto.getDescricao(),
+                    produto.getPreco(),
+                    produto.getQuantidade(),
+                    produto.getCategoria()
+                };
+                modeloTabela.addRow(linha);
+            }
+
+        }catch (SQLException iu){
+            JOptionPane.showMessageDialog(this,"Erro ao carregar produtos: "+iu.getMessage());
+        }
+
+    }
 
     public TelaProduto(){
         setTitle("Cadastro de produtos");
-        setSize(500, 400);
+        setSize(630, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel painelFormulario = new JPanel(new GridLayout(5,2,5,5));
+        JPanel btnPainel = new JPanel();
 
         painelFormulario.add(new JLabel("Nome: "));
         nome_campo = new JTextField();
@@ -44,7 +96,26 @@ public class TelaProduto extends JFrame {
                                             // categoria.toString (o nome), mas por baixo dos panos continua guardando o objeto completo, com o id junto
         painelFormulario.add(comboCategoria);
 
-        add(painelFormulario, BorderLayout.NORTH);
+        String[] colunas = {"ID","Nome","Descrição","Preço","Quantidade","Categoria"};
+        modeloTabela = new DefaultTableModel(colunas,0); // 0 de zero linhas
+        tabelaProduto = new JTable(modeloTabela);
 
+        JScrollPane scrollTabela = new JScrollPane(tabelaProduto);
+
+        add_btn = new JButton("Adicionar");
+        atua_btn = new JButton("Atualizar");
+        excluir_btn = new JButton("Excluir");
+        limpar_btn = new JButton("Limpar");
+
+        btnPainel.add(add_btn);
+        btnPainel.add(atua_btn);
+        btnPainel.add(excluir_btn);
+        btnPainel.add(limpar_btn);
+
+        add(painelFormulario, BorderLayout.NORTH);
+        add(scrollTabela, BorderLayout.CENTER);
+        add(btnPainel, BorderLayout.SOUTH);
+
+        carregarCategoria();
     }
 }
